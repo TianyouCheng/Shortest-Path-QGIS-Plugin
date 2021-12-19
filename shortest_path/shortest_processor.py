b2906afe027690a2bf3675eff65698e1d68e99bc
kuuhakuJ <jiangjinting@pku.edu.cn>
2021-12-19 23:43:57 +0800

    数据预处理

--- shortest_path/shortest_processor.py
+++ shortest_path/shortest_processor.py
@@ -16,26 +16,41 @@ def read_geotiff(filename):
     col=tiff.RasterYSize
     band=tiff.RasterCount
     geoTransform=tiff.GetGeoTransform()
-    data=np.zeros([row,col,band])
-    for i in range(band):
-        dt=tiff.GetRasterBand(1+i)
-        data[:,:,i]=dt.ReadAsArray(0,0,row,col)
-    return data
+    data=tiff.ReadAsArray(0,0,row,col)
+    proj=tiff.GetProjection()
+    return data,row,col,band,geoTransform,proj
 
+def write_geotiff(filename,data,**kwargs):
+    driver=gdal.GetDriverByName('GTiff')
+    dataset=driver.Create(filename,kwargs['row'],kwargs['col'],1,gdal.GDT_UInt32)
+    dataset.SetGeoTransform(kwargs['geoTransform'])
+    dataset.SetProjection(kwargs['proj'])
+    dataset.GetRasterBand(1).WriteArray(data)
+    del dataset
     
 
 if __name__=='__main__':
-    filename=op.join(sys.path[0],'data/dem.tif')
+    filename=op.join(sys.path[0],'data/dem_new.tif')
     cost_raster=read_geotiff(filename)
-    rslt=dijkstra.dijkstra(((2550,540),),(((2586,773),),),cost_raster,True)
-    color1 = colorConverter.to_rgba('white',alpha=0.0)
-    color2 = colorConverter.to_rgba('red',alpha=1)
-    cmap2 = mpl.colors.LinearSegmentedColormap.from_list('my_cmap2',[color1,color2],256)
-    path_np=np.zeros(cost_raster.shape,dtype=np.uint8)
+    # rslt=dijkstra.dijkstra(((2550,540),),(((2586,773),),),cost_raster,True)
+    # color1 = colorConverter.to_rgba('white',alpha=0.0)
+    # color2 = colorConverter.to_rgba('red',alpha=1)
+    # cmap2 = mpl.colors.LinearSegmentedColormap.from_list('my_cmap2',[color1,color2],256)
+    # path_np=np.zeros(cost_raster.shape,dtype=np.uint8)
     
-    for path in rslt[0][0]:
-        path_np[path[1],path[0]]=10000
-    print(rslt)
-    plt.imshow(cost_raster)
-    plt.imshow(path_np,cmap=cmap2)
-    plt.show()
+    # for path in rslt[0][0]:
+    #     path_np[path[1],path[0]]=10000
+    # print(rslt)
+    # plt.imshow(cost_raster)
+    # plt.imshow(path_np,cmap=cmap2)
+    # plt.show()
+    data=cost_raster[0].astype(np.int32)
+    data=(data+data.min())**2
+    write_geotiff(
+        op.join(sys.path[0],'data/dem_new.tif'),data,
+        row=cost_raster[1],
+        col=cost_raster[2],
+        geoTransform=cost_raster[4],
+        proj=cost_raster[5]
+    )
+    print(1)
