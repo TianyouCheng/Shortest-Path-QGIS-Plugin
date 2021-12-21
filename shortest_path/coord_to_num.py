6123a2ac43d6b809684319282985eec6cb7bb46a
fakegiserhyq <1700012457@pku.edu.cn>
2021-12-21 20:08:43 +0800

    Update coord_to_num.py

--- shortest_path/coord_to_num.py
+++ shortest_path/coord_to_num.py
@@ -6,13 +6,14 @@ def coord_to_num(dataset, x, y):
     '''将投影坐标转成行列号(x, y)'''
     geotransform = dataset.GetGeoTransform()
     b = np.array([x - geotransform[0], y - geotransform[3]])
-    A = [[geotransform[1], geotransform[2]], [geotransform[4], geotransform[5]]]
+    A = np.array([[geotransform[1], geotransform[2]], [geotransform[4], geotransform[5]]])
     x_pixel, y_line = [int(i) for i in np.linalg.solve(A, b)]
-    return x_pixel, y_line
+    return  y_line, x_pixel
 
 
 def num_to_coord(dataset, px, line):
     '''将行列号转成投影坐标(lng, lat)或(x, y)'''
+    px, line = line, px
     geotransform = dataset.GetGeoTransform()
     x = geotransform[0] + (px + 0.5) * geotransform[1] + (line + 0.5) * geotransform[2]
     y = geotransform[3] + (px + 0.5) * geotransform[4] + (line + 0.5) * geotransform[5]
